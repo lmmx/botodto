4d7dd2e0420321261e2f2c0c0485ca7f06f072b1
Louis Maddox <louismmx@gmail.com>
2023-06-10 18:26:00 +0100

    Fix test suite nits

--- tests/model_value_checks_test.py
+++ tests/model_value_checks_test.py
@@ -14,7 +14,8 @@ def test_v2_min_value_check():
     assert isinstance(model, v2MinJson)
     assert model.shapes
     name = "Sd"
-    al_shape = next(shape for shape in model.shapes if shape.name == name)
+    al_shape = next((shape for shape in model.shapes if shape.name == name), None)
+    assert al_shape is not None
     # It's not really covered but we don't care
 
 
@@ -30,9 +31,10 @@ def test_v2_norm_ActivityList_value_check():
 def test_v3_value_check():
     model = build_v3()
     assert isinstance(model, v3Json)
-    service_shape = model.shapes[0]
+    first_shape = model.shapes[0]
     service_name = "com.amazonaws.sfn#AWSStepFunctions"
-    al_shape = next(shape for shape in model.shapes if shape.name == service_name)
+    service_shape = next(shape for shape in model.shapes if shape.name == service_name)
+    assert first_shape is service_shape
     assert service_shape.type == "service"
     assert len(service_shape.operations) == 26
     assert service_shape.operations[0].target == "com.amazonaws.sfn#CreateActivity"
