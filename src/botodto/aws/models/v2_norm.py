a5b707215407f6e411767527cb7c1a6184fda640
Louis Maddox <louismmx@gmail.com>
2023-06-10 18:10:30 +0100

    Move dict-to-list pydantic helper func to utils dir

--- src/botodto/aws/models/v2_norm.py
+++ src/botodto/aws/models/v2_norm.py
@@ -5,6 +5,7 @@ from typing import Optional
 from pydantic import BaseModel, root_validator
 
 from ...utils.json_utils import ingest_json
+from ...utils.model_utils import listify_obj
 
 
 class ShapeReference(BaseModel):
@@ -54,17 +55,6 @@ class Operation(BaseModel):
 
 class Operations(BaseModel):
     __root__: dict[str, Operation]
-
-
-def bundle_obj(obj: dict, key_alias: str):
-    return [{key_alias: key, **values} for key, values in obj.items()]
-
-
-def listify_obj(values: dict, target_attr: str, key_alias: str):
-    values[target_attr] = bundle_obj(
-        obj=values.get(target_attr, {}), key_alias=key_alias
-    )
-    return values
 
 
 class v2NormalJson(BaseModel):
---
+++ src/botodto/utils/model_utils.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+__all__ = ["bundle_obj", "listify_obj"]
+
+
+def bundle_obj(obj: dict, key_alias: str):
+    return [{key_alias: key, **values} for key, values in obj.items()]
+
+
+def listify_obj(values: dict, target_attr: str, key_alias: str):
+    values[target_attr] = bundle_obj(
+        obj=values.get(target_attr, {}), key_alias=key_alias
+    )
+    return values
