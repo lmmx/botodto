80a5c9e53be1b455e7da85b1655b37283d8b9266
Louis Maddox <louismmx@gmail.com>
2023-06-11 01:24:29 +0100

    TDD outline for remaining schema v2 (normal) model validation

---
+++ tests/model_build_all_aws_apis_test.py
@@ -0,0 +1,18 @@
+"""
+Tests for complete model integrity.
+"""
+from pytest import mark
+
+from botodto.aws.models.v2_norm import build_model as build_norm
+from botodto.aws.models.v2_norm import v2NormalJson
+from botodto.utils.git.oa_repo import aws_oa_dir, clone_repository
+
+
+@mark.skip(reason="Long running test, do not want to run it in normal suite")
+def test_v2_norm_build():
+    if not aws_oa_dir.exists():
+        clone_repository()
+    # TODO ingest the sources and check the model works for all services
+    raise NotImplementedError
+    model = build_norm()
+    assert isinstance(model, v2NormalJson)
